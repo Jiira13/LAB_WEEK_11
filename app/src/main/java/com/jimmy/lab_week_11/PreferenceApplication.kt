c8f1ee577cdd1319c4c980b07bb7ba482e008be9
Jimagination <jimmytandra65@gmail.com>
2025-11-18 16:25:18 +0700

    Part 1

---
+++ app/src/main/java/com/jimmy/lab_week_11/PreferenceApplication.kt
@@ -0,0 +1,17 @@
+package com.jimmy.lab_week_11
+
+import android.app.Application
+import android.content.Context
+
+class PreferenceApplication : Application() {
+    lateinit var preferenceWrapper: PreferenceWrapper
+    override fun onCreate() {
+        super.onCreate()
+        preferenceWrapper = PreferenceWrapper(
+            getSharedPreferences(
+                    "prefs",
+                    Context.MODE_PRIVATE
+            )
+        )
+    }
+}
